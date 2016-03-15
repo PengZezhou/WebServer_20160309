8077d729eb989fbe03b3bb46d792528fdd51ed69
PengZezhou <pengzhz@succez.com>
2016-03-15 16:08:11 +0800

    增加服务器处理线程

---
+++ HttpServer/src/main/java/com/succez/server/launcher/ServerThread.java
@@ -0,0 +1,25 @@
+package com.succez.server.launcher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.succez.server.connector.Connector;
+import com.succez.server.util.Constant;
+
+public class ServerThread implements Runnable {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ServerThread.class);
+
+	public void run() {
+		if (Constant.SERVER_SOCKET.isClosed()
+				|| Constant.THREAD_POOL.isShutdown()) {
+			return;
+		}
+		Connector connector = new Connector();
+		if (-1 == connector.requestMonitor()) {
+			LOGGER.info("server fail inited");
+		} else {
+			LOGGER.info("server inited");
+		}
+	}
+}
