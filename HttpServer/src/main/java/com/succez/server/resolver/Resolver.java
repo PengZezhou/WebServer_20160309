bf63def3fd8a5e34e5cf7b1cfb2c854c36d9b655
PengZezhou <pengzhz@succez.com>
2016-03-15 19:06:06 +0800

    完成解析器模块和应答器模块主要功能

--- HttpServer/src/main/java/com/succez/server/resolver/Resolver.java
+++ HttpServer/src/main/java/com/succez/server/resolver/Resolver.java
@@ -1,5 +1,6 @@
 package com.succez.server.resolver;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -9,44 +10,97 @@ import org.apache.http.impl.DefaultBHttpServerConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.succez.server.responser.Handler;
+
 public class Resolver {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(Resolver.class);
 	private Socket socket;
-	public Resolver(Socket socket){
+
+	/**
+	 * 构造函数，初始化socket
+	 * 
+	 * @param socket
+	 */
+	public Resolver(Socket socket) {
 		this.socket = socket;
 	}
-	
-	public void urlResolve(){
+
+	/**
+	 * url解析处理，按类型分发到下层处理
+	 */
+	public void urlResolve() {
 		LOGGER.info("开始解析url");
-		DefaultBHttpServerConnection con = new DefaultBHttpServerConnection(1024);
+		DefaultBHttpServerConnection con = null;
 		try {
+			con = new DefaultBHttpServerConnection(1024 * 2);
 			con.bind(socket);
-			
-			 HttpRequest req = con.receiveRequestHeader();
-			 String url = req.getRequestLine().getUri();
-			 System.out.println(url);
+			HttpRequest req = con.receiveRequestHeader();
+			String url = req.getRequestLine().getUri();
+			urlType(url);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.info("解析url，出现IO异常");
 		} catch (HttpException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally{
-			if(con!=null){
+			LOGGER.info("解析url，出现http异常");
+		} finally {
+			if (con != null) {
 				try {
 					con.close();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					LOGGER.info("解析url，http关闭连接异常");
 				}
 			}
 		}
-		assignment();
 	}
-	
-	private void assignment(){
-		
+
+	/**
+	 * 解析url地址，判断请求类型（文件夹、文件、其他）
+	 * 
+	 * @param url
+	 * @return
+	 */
+	private void urlType(String url) {
+		int tmp = 0;
+		File file = null;
+		String[] strs = url.split("/");
+		StringBuilder sb = new StringBuilder();
+		sb.append("D:");
+		for (String str : strs) {
+			sb.append("\\" + str);
+		}
+		file = new File(sb.toString());
+		if (file.isDirectory()) {
+			tmp = 1;
+		} else if (file.isFile()) {
+			tmp = 2;
+		} else if (!file.exists()) {
+			tmp = 3;
+		} else {
+			tmp = 4;
+		}
+		assignment(tmp, file);
+	}
+
+	/**
+	 * 根据类型，调用相应处理模块
+	 * 
+	 * @param flag
+	 */
+	private void assignment(int flag, File file) {
+		String str = null;
+		Handler handler = new Handler(this.socket);
+		switch (flag) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			break;
+		}
+		handler.responseHandler(str);
 	}
 }
--- HttpServer/src/main/java/com/succez/server/responser/Handler.java
+++ HttpServer/src/main/java/com/succez/server/responser/Handler.java
@@ -1,5 +1,54 @@
 package com.succez.server.responser;
 
-public class Handler {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Handler {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Handler.class);
+	private Socket socket;
+
+	/**
+	 * 构造函数
+	 * 
+	 * @param socket
+	 */
+	public Handler(Socket socket) {
+		this.socket = socket;
+	}
+
+	/**
+	 * 响应浏览器
+	 * 
+	 * @param str
+	 *            输出字符串
+	 */
+	public void responseHandler(String str) {
+		byte[] bytes = str.getBytes();
+		OutputStream out = null;
+		try {
+			out = socket.getOutputStream();
+			out.write(bytes);
+		} catch (IOException e) {
+			LOGGER.error("响应异常，IO错误");
+		} finally {
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					LOGGER.warn("socket 输出流关闭错误");
+				}
+			}
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					LOGGER.warn("socket关闭错误");
+				}
+			}
+		}
+	}
 }
