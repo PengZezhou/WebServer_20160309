4a584c38b4023245c96925646c5158b9b4ff6e05
PengZezhou <pengzhz@succez.com>
2016-03-16 19:17:17 +0800

    完善文件下载模块

--- HttpServer/src/main/java/com/succez/server/downloader/FileDownload.java
+++ HttpServer/src/main/java/com/succez/server/downloader/FileDownload.java
@@ -1,9 +1,85 @@
 package com.succez.server.downloader;
 
+import java.io.Closeable;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FileDownload {
-	public void downloadFile(File file){
-		
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(FileDownload.class);
+
+	private Socket socket;
+
+	/**
+	 * 构造函数
+	 * 
+	 * @param socket
+	 */
+	public FileDownload(Socket socket) {
+		this.socket = socket;
+	}
+
+	/**
+	 * 下载指定路径的文件
+	 * 
+	 * @param file
+	 */
+	public void downloadFile(File file) {
+		LOGGER.info("开始执行下载");
+		FileInputStream fis = null;
+		OutputStream out = null;
+		FileChannel fileChannel = null;
+		try {
+			out = socket.getOutputStream();
+			fis = new FileInputStream(file);
+			fileChannel = fis.getChannel();
+
+			ByteBuffer bf = ByteBuffer.allocate(786432);
+			byte[] bytes = new byte[131072];
+			int nRead, nGet;
+			while ((nRead = fileChannel.read(bf)) != -1) {
+				if (nRead == 0) {
+					continue;
+				}
+				bf.flip();
+				while (bf.hasRemaining()) {
+					nGet = Math.min(bf.remaining(), 131072);
+					bf.get(bytes, 0, nGet);
+					out.write(bytes);
+				}
+				bf.clear();
+			}
+		} catch (IOException e) {
+			LOGGER.error(" 文件下载，NIO出现异常");
+		} finally {
+			closeStream(fis);
+			closeStream(out);
+			closeStream(fileChannel);
+		}
+		LOGGER.info("开始下载结束");
+	}
+
+	/**
+	 * 内部方法，关闭流的逻辑封装
+	 * 
+	 * @param closeavle
+	 *            流对象
+	 */
+	private static final void closeStream(Closeable closeavle) {
+		if (closeavle != null) {
+			try {
+				closeavle.close();
+			} catch (IOException e) {
+				LOGGER.error(closeavle.toString() + " 流关闭出现异常");
+			}
+		}
 	}
 }
--- HttpServer/src/main/java/com/succez/server/resolver/Resolver.java
+++ HttpServer/src/main/java/com/succez/server/resolver/Resolver.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.succez.server.directory.DirectoryInfo;
+import com.succez.server.downloader.FileDownload;
 import com.succez.server.reader.ReadFile;
 import com.succez.server.responser.Handler;
 import com.succez.server.util.Constant;
@@ -20,6 +21,7 @@ public class Resolver {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(Resolver.class);
 	private Socket socket;
+	private DefaultBHttpServerConnection con = null;
 
 	/**
 	 * 构造函数，初始化socket
@@ -35,7 +37,6 @@ public class Resolver {
 	 */
 	public void urlResolve() {
 		LOGGER.info("开始解析url");
-		DefaultBHttpServerConnection con = null;
 		try {
 			con = new DefaultBHttpServerConnection(Constant.SERVER_CHACHE);
 			con.bind(socket);
@@ -98,24 +99,28 @@ public class Resolver {
 		case 1:
 			DirectoryInfo d = new DirectoryInfo();
 			str = d.listFromDirectory(file);
+			handler.responseHandler(str);
 			break;
 		case 2:
 			ReadFile r1 = new ReadFile();
 			str = r1.convertFromFile(file);
+			handler.responseHandler(str);
 			break;
 		case 3:
 			ReadFile r2 = new ReadFile();
 			str = r2.convertFromFile(new File(Constant.ERROR_404));
+			handler.responseHandler(str);
 			break;
 		case 4:
-			str = "文件将被下载...";
+			FileDownload f = new FileDownload(socket);
+			f.downloadFile(file);
 			break;
 		default:
 			ReadFile r3 = new ReadFile();
 			str = r3.convertFromFile(new File(Constant.ERROR_500));
+			handler.responseHandler(str);
 			break;
 		}
-		handler.responseHandler(str);
 	}
 
 	/**
@@ -128,6 +133,9 @@ public class Resolver {
 	private Boolean fileType(File file) {
 		String name = file.getName();
 		int dot = name.lastIndexOf('.');
+		if (dot == -1) {
+			return false;
+		}
 		String ext = name.substring(dot, name.length()).toLowerCase();
 		String[] arr = Constant.EXTEN_NAME.split("\\|");
 		for (String str : arr) {
